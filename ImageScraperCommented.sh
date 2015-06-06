088178f79d1aecf67b6ea2da129b24dcdf15ccb4
Marcello Hunt <thefowlmail@gmail.com>
2015-06-06 12:38:41 +0100

    Reorganised the comments

--- ImageScraperCommented.sh
+++ ImageScraperCommented.sh
@@ -3,42 +3,55 @@
 #Image Scraper, pulls all images from a given website
 
 #Variables
-site='www.tickld.com/all/hot' #Place holder web page
-recursive= #Recursive is declared but not set
+
+ #Place holder web page
+site='www.tickld.com/all/hot'
+#Recursive is declared but not set
+recursive= 
 
 #Function
 usage(){
-  echo "Usage: ImageScraper [ -d Directory Name ] [ -r ] [ -w Website URL ]" #Sample usage string returned when -h | --help is used
+  #Sample usage string returned when -h | --help is used
+  echo "Usage: ImageScraper [ -d Directory Name ] [ -r ] [ -w Website URL ]" 
 }
+#If there is no ImagePull directory then one is created
+[ -d ImagePull ] || mkdir ImagePull 
 
-[ -d ImagePull ] || mkdir ImagePull #If there is no ImagePull directory then one is created
+#Change directory to ImagePull
+cd ImagePull 
 
-cd ImagePull #Change directory to ImagePull
-
-while [ "$1" != "" ]; do #While there are command line arguements
+#While there are command line arguements
+while [ "$1" != "" ]; do 
   case "$1" in
-    -d | --directory )  shift #Creates a new directory with give name and switches to it
+  #Creates a new directory with give name and switches to it
+    -d | --directory )  shift 
                         mkdir $1
                         cd $1
                         ;;
     -h | --help )       usage #Gives help
                         exit
                         ;;
-    -r | --recursive )  recursive=1 #Sets recursive to 1
+  #Sets recursive to 1
+    -r | --recursive )  recursive=1 
                         ;;
-    -w | --website )    shift #Changes site variable to given URL
+  #Changes site variable to given URL                      
+    -w | --website )    shift 
                         site=$1
                         ;;
-    * )                 usage #Unknown arguement so usage is called
+  #Unknown arguement so usage is called                      
+    * )                 usage 
                         exit 1
   esac
   shift
 done
 
-if [ "$recursive" = "1" ]; then #This searches site recursively if recursive is 1
+#This searches site recursively if recursive is 1
+if [ "$recursive" = "1" ]; then 
   wget -r -nd -H -p -A jpg,jpeg,png,gif -e robots=off "$site"
-else #this is the standard one page image scrape
+else 
+  #this is the standard one page image scrape
   wget -nd -H -p -A jpg,jpeg,png,gif -e robots=off "$site"
 fi
 
-ls -l -h | sort | less #This line creates a sorted list of all the images and pipes them into a less to allow easy reading of all the images you just scraped
+#This line creates a sorted list of all the images and pipes them into a less to allow easy reading of all the images you just scraped
+ls -l -h | sort | less 
